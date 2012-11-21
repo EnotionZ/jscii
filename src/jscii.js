b131b8878fd891419c3cb41185e1ac244541697f
Dominick Pham <dominick.pham@gmail.com>
2012-11-21 14:45:04 -0500

    updated api, support for video and color ascii

--- src/jscii.js
+++ src/jscii.js
@@ -1,16 +1,15 @@
+/**
+ * Jscii - Image to ASCII converter
+ * http://enotionz.github.com/jscii/
+ * Author: Dominick Pham (@enotionz | http://dph.am)
+ */
+
 !function() {
 
-	navigator.getMedia = navigator.getUserMedia ||
+	navigator.getUserMedia = navigator.getUserMedia ||
 		navigator.webkitGetUserMedia ||
 		navigator.mozGetUserMedia ||
 		navigator.msGetUserMedia;
-
-	var imgCanvas = document.createElement('canvas');
-	var imgCtx = imgCanvas.getContext('2d');
-	var videoCanvas = document.createElement('canvas');
-	var videoCtx = videoCanvas.getContext('2d');
-
-	var videoWidth, videoHeight, video, container, stream, videoTimer;
 
 	/**
 	 * value to character mapping from dark to light
@@ -27,103 +26,124 @@
 	function logError(err) { if(console && console.log) console.log('Error!', err); return false; }
 
 	/**
-	 * Sets the video dimension (and subsequently ASCII string dimension)
+	 * Options:
+	 * el        - DOM node (img or video)
+	 * container - if supplied, ascii string will automatically be set on container innerHTML during a render
+	 * fn        - function, callback to fire during a render with ascii string as arguments[0]
+	 * width     - hi-res images/videos must be resized down, specify width and jscii will figure out height
+	 * color     - enable color ascii (highly experimental)
+	 * interval  - integer - for videos only, this is the interval between each render
+	 * webrtc    - bool, default false, only applicable if 'el' is a video
 	 */
-	function setVideoDimension(width, height) {
-		videoCanvas.width = videoWidth = width;
-		videoCanvas.height = videoHeight = height;
-	}
+	function Jscii(params) {
+		var self = this;
 
-	/**
-	 * given a video object and DOM element, render the ASCII string inside element
-	 */
-	function renderVideo(videoEl, containerEl) {
-		if(typeof navigator.getMedia !== 'function') {
-			var msg = 'Error: browser does not support getUserMedia';
-			containerEl.innerHTML = msg;
-			return logError(msg);
-		}
+		var el = this.el = params.el;
+		this.container = params.container;
+		this.fn = typeof params.fn === 'function' ? params.fn : null;
+		this.width = typeof params.width === 'number' ? params.width : 150;
+		this.color = !!params.color;
 
-		video = videoEl;
-		container = containerEl;
-		navigator.getMedia({video: true, audio: true}, function(localMediaStream){
-			stream = localMediaStream;
-			var url = window.URL || window.webkitURL;
-			video.src = url.createObjectURL(localMediaStream);
+		this.canvas = document.createElement('canvas');
+		this.ctx = this.canvas.getContext('2d');
 
-			startRender(15);
-			video.onloadedmetadata = logError;
-		}, logError);
-		return true;
-	}
+		var nodeName = el.nodeName;
+		if(nodeName === 'IMG') {
+			el.addEventListener('load', function(){ self.render(); });
+		} else if(nodeName === 'VIDEO') {
+			this.interval = typeof params.interval === 'number' ? params.interval : 15;
+			this.webrtc = !!params.webrtc;
 
-	/**
-	 * gets video image data, perform ascii conversion, append string to container
-	 */
-	function startRender(interval) {
-		if(typeof interval !== 'number') interval = 20;
-		videoTimer = setInterval(function(){
-			if(stream) {
-				var w = videoWidth, h = videoHeight;
-				videoCtx.drawImage(video, 0, 0, w, h);
-				var data = videoCtx.getImageData(0, 0, w, h).data;
-				container.innerHTML = getAsciiString(data, w, h);
+			if(this.webrtc) {
+				if(typeof navigator.getUserMedia !== 'function') {
+					return logError((el.innerHTML = 'Error: browser does not support WebRTC'));
+				}
+				navigator.getUserMedia({video: true, audio: false}, function(localMediaStream){
+					self.mediaStream = localMediaStream;
+					el.src = (window.URL || window.webkitURL).createObjectURL(localMediaStream);
+				}, logError);
 			}
-		}, interval);
+			el.addEventListener('loadeddata', function() { self.play(); });
+		}
 	}
 
 	/**
-	 * Allow pause and play for ascii rendering
+	 * start rendering, for video type only
 	 */
-	function stopRender() { if(videoTimer) clearInterval(videoTimer); }
+	Jscii.prototype.play = function() {
+		var self = this;
+		self.pause().videoTimer = setInterval(function() {
+			if(self.mediaStream || !self.webrtc) self.render();
+		}, self.interval);
+		return self;
+	};
 
 	/**
-	 * given an image object and DOM element, render the ASCII string inside element
+	 * pause rendering, for video type only
 	 */
-	function renderImage(image, container) {
-		image.addEventListener('load', function(){
-			var ratio = image.width/image.height;
-			imgCanvas.width = w = 150;
-			imgCanvas.height = h = w/ratio;
-			imgCtx.drawImage(image, 0, 0, w, h);
-			data = imgCtx.getImageData(0, 0, w, h).data;
-			container.innerHTML = getAsciiString(data, w, h);
-		});
-	}
+	Jscii.prototype.pause = function() {
+		if(this.videoTimer) clearInterval(this.videoTimer);
+		return this;
+	};
 
 	/**
-	 * helper function to retrieve rgb value from pixel data
-	 * (pixel data is a 1-dimensional array of rgba sequence)
+	 * getter/setter for output dimension
 	 */
-	function getRGB(d, i) { return [d[i=i*4], d[i+1], d[i+2]]; }
+	Jscii.prototype.dimension = function(width, height) {
+		if(typeof width === 'number' && typeof height === 'number') {
+			this._scaledWidth = this.canvas.width = width;
+			this._scaledHeight = this.canvas.height = height;
+			return this;
+		} else {
+			return { width: this._scaledWidth, height: this._scaledHeight };
+		}
+	};
+
+	/**
+	 * gets context image data, perform ascii conversion, append string to container
+	 */
+	Jscii.prototype.render = function() {
+		var el = this.el, nodeName = el.nodeName, ratio;
+		var dim = this.dimension(), width, height;
+		if(!dim.width || !dim.height) {
+			ratio = nodeName === 'IMG' ? el.height/el.width : el.videoHeight/el.videoWidth;
+			this.dimension(this.width, parseInt(this.width*ratio, 10));
+			dim = this.dimension();
+		}
+		width = dim.width;
+		height = dim.height;
+
+		// might take a few cycles before we
+		if(!width || !height) return;
+
+		this.ctx.drawImage(this.el, 0, 0, width, height);
+		this.imageData = this.ctx.getImageData(0, 0, width, height).data;
+		var asciiStr = this.getAsciiString();
+		if(this.container) this.container.innerHTML = asciiStr;
+		if(this.fn) this.fn(asciiStr);
+	};
 
 	/**
 	 * given a picture/frame's pixel data and a defined width and height
 	 * return the ASCII string representing the image
 	 */
-	function getAsciiString(d, width, height) {
-		var len = width*height-1, str = '';
+	Jscii.prototype.getAsciiString = function() {
+		var dim = this.dimension(), width = dim.width, height = dim.height;
+		var len = width*height-1, d = this.imageData, str = '';
+
+		// helper function to retrieve rgb value from pixel data
+		var getRGB = function(i) { return [d[i=i*4], d[i+1], d[i+2]]; };
+
 		for(var i=0; i<len; i++) {
-			if(i%width === 0) str += '<br>';
-			var rgb = getRGB(d, i);
+			if(i%width === 0) str += '\n';
+			var rgb = getRGB(i);
 			var val = Math.max(rgb[0], rgb[1], rgb[2])/255;
-			//str += '<b style="color: rgb('+rgb.join(',')+')">'+getChar(val)+'</b>';
-			str += getChar(val);
+			if(this.color) str += '<font style="color: rgb('+rgb.join(',')+')">'+getChar(val)+'</font>';
+			else str += getChar(val);
 		}
 		return str;
-	}
-
-	/**
-	 * default video dimension at 150 width and a 4:3 ratio
-	 */
-	setVideoDimension(150, parseInt(150*3/4, 10));
-
-	window.Jscii = {
-		setVideoDimension: setVideoDimension,
-		renderVideo: renderVideo,
-		startRender: startRender,
-		stopRender: stopRender,
-		renderImage: renderImage
 	};
+
+	window.Jscii = Jscii;
 
 }();
